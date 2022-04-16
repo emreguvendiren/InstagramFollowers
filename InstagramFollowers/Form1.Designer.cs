0020168753a15ffaae15c05aeb6529a7a94e34ff
Emre Güvendiren <guvendirenemre1@hotmail.com>
2022-04-16 03:11:42 +0300

    a

--- InstagramFollowers/Form1.Designer.cs
+++ InstagramFollowers/Form1.Designer.cs
@@ -29,20 +29,80 @@ namespace InstagramFollowers
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(212, 56);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(384, 47);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(212, 142);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(384, 47);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(678, 75);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(255, 81);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Giriş";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2252, 1206);
+            this.ClientSize = new System.Drawing.Size(1637, 788);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserName);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "İnstaFollowers";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- InstagramFollowers/Form1.cs
+++ InstagramFollowers/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,11 @@ namespace InstagramFollowers
 {
     public partial class Form1 : Form
     {
+        IWebDriver driver;
+        string username;
+        List<String> Followers = new List<String>();
+        List<String> Follows = new List<String>();
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +27,33 @@ namespace InstagramFollowers
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //driver = new ChromeDriver();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            username = txtUserName.Text;
+            string password = txtPassword.Text;
+            if (username == null || username == "" || password == null || password == "")
+            {
+                MessageBox.Show("Gecersiz kullanici adi veya sifre!");
+            }
+            else
+            {
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://www.instagram.com");
+                IWebElement userNameInstagram = driver.FindElement(By.Name("username"));
+                IWebElement passwordInstagram = driver.FindElement(By.Name("password"));
+
+                userNameInstagram.SendKeys(username);
+                passwordInstagram.SendKeys(password);
+
+
+            }
         }
     }
 }
