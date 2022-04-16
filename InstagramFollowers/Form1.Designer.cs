085bdbaabc5a25eba0348f879dc65726446ca52a
Emre Güvendiren <guvendirenemre1@hotmail.com>
2022-04-16 04:30:30 +0300

    a

--- InstagramFollowers/Form1.Designer.cs
+++ InstagramFollowers/Form1.Designer.cs
@@ -34,6 +34,12 @@ namespace InstagramFollowers
             this.label2 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtUserName
@@ -65,6 +71,7 @@ namespace InstagramFollowers
             // 
             this.txtPassword.Location = new System.Drawing.Point(212, 142);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(384, 47);
             this.txtPassword.TabIndex = 3;
             // 
@@ -78,11 +85,72 @@ namespace InstagramFollowers
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 41;
+            this.listBox1.Location = new System.Drawing.Point(183, 280);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(296, 496);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(183, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 41);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Takip Etmediklerin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(632, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 41);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Takip Etmeyenler";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 41;
+            this.listBox2.Location = new System.Drawing.Point(637, 280);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(296, 496);
+            this.listBox2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1259, 447);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 41);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1250, 575);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 58);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1637, 788);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label2);
@@ -103,6 +171,12 @@ namespace InstagramFollowers
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- InstagramFollowers/Form1.cs
+++ InstagramFollowers/Form1.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,8 +17,11 @@ namespace InstagramFollowers
     {
         IWebDriver driver;
         string username;
-        List<String> Followers = new List<String>();
-        List<String> Follows = new List<String>();
+        List<string> Followers = new List<string>();
+        List<string> Follows = new List<string>();
+        List<string> notFollow = new List<string>();
+        List<string> notFollowers = new List<string>();
+        string deneme;
 
         public Form1()
         {
@@ -32,6 +36,10 @@ namespace InstagramFollowers
         private void Form1_Load_1(object sender, EventArgs e)
         {
             //driver = new ChromeDriver();
+            listBox1.Visible = false;
+            label3.Visible = false;
+            listBox2.Visible = false;
+            label4.Visible = false;
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
@@ -46,14 +54,132 @@ namespace InstagramFollowers
             {
                 driver = new ChromeDriver();
                 driver.Navigate().GoToUrl("https://www.instagram.com");
+                Thread.Sleep(5000);
                 IWebElement userNameInstagram = driver.FindElement(By.Name("username"));
                 IWebElement passwordInstagram = driver.FindElement(By.Name("password"));
+                IWebElement loginBtn = driver.FindElement(By.CssSelector(".sqdOP.L3NKy.y3zKF"));
+
 
                 userNameInstagram.SendKeys(username);
                 passwordInstagram.SendKeys(password);
+                Thread.Sleep(2000);
+                loginBtn.Click();
+                Thread.Sleep(7000);
+                driver.Navigate().GoToUrl("https://www.instagram.com/" + username);
+                Thread.Sleep(2000);
+
+                IWebElement follows = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(2) > a"));
+                follows.Click();
+                Thread.Sleep(1000);
+                string jsCommand = "" +
+                "sayfa = document.querySelector('.isgrP');" +
+                "sayfa.scrollTo(0,sayfa.scrollHeight);" +
+                "var sayfaSonu = sayfa.scrollHeight;" +
+                "return sayfaSonu;";
+
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                var sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
+
+                while (true)
+                {
+                    var son = sayfaSonu;
+                    Thread.Sleep(1100);
+                    sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
+                    if (son == sayfaSonu)
+                        break;
+                }
+
+                IReadOnlyCollection<IWebElement> TakipEdilenler = driver.FindElements(By.CssSelector(".notranslate._0imsa"));
+                foreach (IWebElement item in TakipEdilenler)
+                {
+                    Follows.Add(item.Text);
+                }
+                Thread.Sleep(500);
+
+                IWebElement close = driver.FindElement(By.XPath("/html/body/div[6]/div/div/div/div[1]/div/div[3]/div/button"));
+                close.Click();
+                Thread.Sleep(500);
+                //#react-root > section > main > div > header > section > ul > li:nth-child(3) > a
+                IWebElement followers = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a"));
+                followers.Click();
+                Thread.Sleep(1000);
+
+                var sayfaSonu2 = Convert.ToInt32(js.ExecuteScript(jsCommand));
+
+                while (true)
+                {
+                    var son = sayfaSonu2;
+                    Thread.Sleep(1100);
+                    sayfaSonu2 = Convert.ToInt32(js.ExecuteScript(jsCommand));
+                    if (son == sayfaSonu2)
+                        break;
+                }
+
+                IReadOnlyCollection<IWebElement> Takipciler = driver.FindElements(By.CssSelector(".notranslate._0imsa"));
+                foreach (IWebElement item in Takipciler)
+                {
+                    Followers.Add(item.Text);
+                }
+                Thread.Sleep(500);
+
+                foreach (var follow in Follows)
+                {
+                    int sayac = 0;
+                    foreach (var followrs in Followers)
+                    {
+                        if (follow == followrs)
+                        {
+                            sayac = 1;
+                        }
+                    }
+                    if (sayac == 0)
+                    {
+                        notFollow.Add(follow);
+                    }
+                }
+                foreach (var followrs in Followers)
+                {
+                    int sayac = 0;
+                    foreach (var follow in Follows)
+                    {
+                        if (followrs == follow)
+                        {
+                            sayac = 1;
+                        }
+                    }
+                    if (sayac == 0)
+                    {
+                        notFollowers.Add(followrs);
+                    }
+                }
+
+                listBox1.Visible = true;
+                label3.Visible = true;
+                listBox2.Visible = true;
+                label4.Visible = true;
+                int sayac2 = 1;
+                foreach (var item in notFollow)
+                {
+                    listBox1.Items.Add(sayac2+"->"+item);
+                    sayac2++;
+                }
+                int sayac3 = 1;
+                foreach (var item in notFollowers)
+                {
+                    listBox2.Items.Add(sayac3 + "->" + item);
+                    sayac3++;
+                }
 
 
             }
+        }
+        public static void scrollToBack() {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = listBox2.SelectedItem.ToString();
         }
     }
 }
